c14ac12babc2a2bd05b8684b832276e4763f1adc
Christopher Badi AbdulWahid <cabdulwa@opteron.crc.nd.edu>
2009-10-07 18:29:33 -0400

    temporary commit

--- UniprotXmlParser.hs
+++ UniprotXmlParser.hs
@@ -25,18 +25,33 @@ data Lineage = Lin {
       taxonomy :: [Taxonomy]
     } deriving (Eq, Show)
 
+
+data OrganismName = Scientific String
+                  | Common String
+                  | Synonym String
+                  | Unknown String
+                    deriving (Eq, Show)
+
+mkOrganismName "scientific" = Scientific
+mkOrganismName "common" = Common
+mkOrganismName "synonym" = Synonym
+mkOrganismName _ = Unknown
+
 data Organism = Org {
-      scientificOrgName
-    , commonOrgName
-    , synonymOrgName :: String
+      orgNames :: [OrganismName]
     , orgLineage :: Lineage
+    , orgDbRefs :: [DBRef]
     } deriving (Eq, Show)
+
+type OrganismHost = Organism
 
 data DBRef = DBRef {
       dbRefType :: String
     , dbRefKey :: String
     , dbRefId :: String
     } deriving (Eq, Show)
+
+mkDBRef t k i = DBRef {dbRefType = t, dbRefKey = k, dbRefId = i}
 
 data UniprotEntry = UEntry {
       accessionUE :: String
@@ -55,13 +70,15 @@ downFrom top a = proc e -> do
 extractFrom mk top a  = arr mk <<< tagData <<< downFrom top a
 extractFromEntry mk = extractFrom mk "entry"
 
+
 entry = atTag "entry" >>>
         proc e -> do
           acc   <- accession   -< e
           ename <- entryName   -< e
           p     <- proteinName -< e
           g     <- gene        -< e
-          returnA -< (acc, g)
+          o     <- organism    -< e
+          returnA -< (acc, o)
           -- returnA -< (acc, ename, p, g)
 
 
@@ -76,12 +93,16 @@ gene = n &&& t >>> arr (uncurry mkGene)
     where n = extractFromEntry id $ hasName "gene" /> hasName "name"
           t = getAttrValue "type" <<< downFrom "entry" (hasName "gene" /> hasName "name")
 
+organismNameType = getAttrValue "type" &&& getText >>> arr (uncurry mkOrganismName)
 
+dbReference = getAttrValue "type" &&& getAttrValue "key" &&& getAttrValue "id" >>>
+              arr (\(t, (k,i)) -> mkDBRef t k i)
 
+organism = hasName "entry" /> hasName "organism" >>> multi organismNameType
 
 
 main = do
-  let xml = "Research/uniprot/uniprot_sprot.xml.top5"
+  let xml = "Research/uniprot.git/uniprot_sprot.xml.top5"
   h <- getHomeDirectory
   ds <- runX (readDocument [] (h </> xml) >>> entry)
   mapM_ print ds
