28a4e56066de46c10746f47fe9200883a1bb5965
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-10-30 14:39:30 -0400

    get single protein/fullName; remove old commented code

--- UniprotXmlParser.hs
+++ UniprotXmlParser.hs
@@ -5,6 +5,7 @@
   #-}
 module Main where
 
+import Control.Parallel.Strategies
 import Control.Applicative
 import Text.XML.HXT.Arrow
 import Text.Printf
@@ -12,60 +13,6 @@ import System.Directory
 import System.Environment
 import System.FilePath
 
--- data Protein
-
--- data ProteinName = Recommended | Submitted deriving (Eq, Show)
-
--- data Gene = Gene {
---       geneType
---     , geneName
---       :: String
---     } deriving (Eq, Show)
-
--- mkGene t n = Gene {geneType = t, geneName = n}
-
--- type Taxonomy = String
-
--- data Lineage = Lin {
---       taxonomy :: [Taxonomy]
---     } deriving (Eq, Show)
-
-
--- data OrganismName = Scientific String
---                   | Common String
---                   | Synonym String
---                   | Unknown String
---                     deriving (Eq, Show)
-
--- mkOrganismName "scientific" = Scientific
--- mkOrganismName "common" = Common
--- mkOrganismName "synonym" = Synonym
--- mkOrganismName _ = Unknown
-
--- data Organism = Org {
---       orgNames :: [OrganismName]
---     , orgLineage :: Lineage
---     , orgDbRefs :: [DBRef]
---     } deriving (Eq, Show)
-
--- type OrganismHost = Organism
-
--- data DBRef = DBRef {
---       dbRefType :: String
---     , dbRefKey :: String
---     , dbRefId :: String
---     } deriving (Eq, Show)
-
--- mkDBRef t k i = DBRef {dbRefType = t, dbRefKey = k, dbRefId = i}
-
--- data UniprotEntry = UEntry {
---       accessionUE :: String
---     , nameUE :: String
---     , proteinUE :: Protein
---     , geneUE :: Gene
---     , organismUE :: Organism
---     , dbreferencesUE :: [DBRef]
---     }
 
 atTag tag = deep (isElem >>> hasName tag)
 atTag' tag = isElem >>> hasName tag
@@ -85,31 +32,23 @@ entry = atTag "entry" >>>
 
 
 
-accession = extractFromEntry id $ hasName "accession"
+accession = single <<< extractFromEntry id $ hasName "accession"
 entryName = extractFromEntry id $ hasName "name"
 proteinName = proc entry -> do
-                e <- getChildren <<< getChildren <<< (hasName "entry" /> hasName "protein") -< entry
+                e <- single fullNames -< entry
+--                 e <- getName <<< getChildren <<< (hasName "entry" /> hasName "protein") -< entry
+--                 s <- tagData  <<< deepest (hasName "fullName") -< e
+                returnA -< e
 
-                s <- tagData  <<< deepest (hasName "fullName") -< e
-                returnA -< s
+fullNames = tagData <<< atTag "fullName" <<< hasName "entry" /> hasName "protein"
+-- recommendedFullName = hasName "entry" /> hasName "protein" /> hasName "recommendedName" /> hasName "fullName"
 
 
 
-
--- gene = n &&& t >>> arr (uncurry mkGene)
---     where n = extractFromEntry id $ hasName "gene" /> hasName "name"
---           t = getAttrValue "type" <<< downFrom "entry" (hasName "gene" /> hasName "name")
-
--- organismNameType = getAttrValue "type" &&& getText >>> arr (uncurry mkOrganismName)
-
--- dbReference = getAttrValue "type" &&& getAttrValue "key" &&& getAttrValue "id" >>>
---               arr (\(t, (k,i)) -> mkDBRef t k i)
-
--- organism = hasName "entry" /> hasName "organism" >>> multi organismNameType
 
 
 showResult (acc_No, e_name, full_name) =
-    printf "%-8s;%-15s;%s\n" acc_No e_name full_name
+    printf "%s;%s;%s\n" acc_No e_name full_name
 
 printResults rs = do
 --   showResult ("Acc#", "Entry Name", "Full name")
@@ -121,10 +60,9 @@ printFullName (_, _, n) = putStrLn n
 main = do
   accessionNos <- words <$> getContents
 --   let accessionNos = ["P25098"]
-  let xmls = map (\no -> "http://www.uniprot.org/uniprot/" ++ no ++ ".xml") accessionNos
-  ns <- (map head) `fmap` mapM (\xml -> runX (readDocument [] xml >>> entry)) xmls
-  printResults ns
---   mapM_ printFullName n
+  let xmls = parMap rwhnf (\no -> "http://www.uniprot.org/uniprot/" ++ no ++ ".xml") accessionNos
+  ns <- concat `fmap` mapM (\xml -> runX (readDocument [] xml >>> entry)) xmls
+  mapM_ showResult ns
 
 
 
