87d32ddb23f9bfb67cd4da86c69ed598b6a98276
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-11-02 11:26:10 -0500

    operate directly on data in the form of 'P13824 P25098 = 3.82730000e-01'

--- UniprotXmlParser.hs
+++ UniprotXmlParser.hs
@@ -1,17 +1,14 @@
 {-# LANGUAGE
   Arrows
-  , EmptyDataDecls
-  , NoMonomorphismRestriction
   #-}
 module Main where
 
-import Control.Parallel.Strategies
 import Control.Applicative
-import Text.XML.HXT.Arrow
-import Text.Printf
-import System.Directory
+import Control.Monad (sequence)
+import Control.Parallel.Strategies (parMap, rwhnf)
+import Data.List (intercalate)
 import System.Environment
-import System.FilePath
+import Text.XML.HXT.Arrow
 
 
 atTag tag = deep (isElem >>> hasName tag)
@@ -28,41 +25,29 @@ entry = atTag "entry" >>>
           acc   <- accession   -< e
           ename <- entryName   -< e
           p     <- proteinName -< e
-          returnA -< (acc, ename, p)
-
+          returnA -< [acc, ename, p]
 
 
 accession = single <<< extractFromEntry id $ hasName "accession"
 entryName = extractFromEntry id $ hasName "name"
 proteinName = proc entry -> do
                 e <- single fullNames -< entry
---                 e <- getName <<< getChildren <<< (hasName "entry" /> hasName "protein") -< entry
---                 s <- tagData  <<< deepest (hasName "fullName") -< e
                 returnA -< e
 
 fullNames = tagData <<< atTag "fullName" <<< hasName "entry" /> hasName "protein"
--- recommendedFullName = hasName "entry" /> hasName "protein" /> hasName "recommendedName" /> hasName "fullName"
 
 
+uniprot id = "http://www.uniprot.org/uniprot/" ++ id ++ ".xml"
 
-
-
-showResult (acc_No, e_name, full_name) =
-    printf "%s;%s;%s\n" acc_No e_name full_name
-
-printResults rs = do
---   showResult ("Acc#", "Entry Name", "Full name")
-  mapM_ showResult rs
-
-printFullName (_, _, n) = putStrLn n
-
+work :: [String] -> IO String
+work [ori,tar,_,prob] = let ori' = uniprot ori
+                            tar' = uniprot tar
+                            !res = parMap rwhnf (\xml -> runX (readDocument [] xml >>> entry)) [ori', tar']
+                        in ((++ ";" ++ prob) . intercalate ";" . concat . concat) <$> sequence res
 
 main = do
-  accessionNos <- words <$> getContents
---   let accessionNos = ["P25098"]
-  let xmls = parMap rwhnf (\no -> "http://www.uniprot.org/uniprot/" ++ no ++ ".xml") accessionNos
-  ns <- concat `fmap` mapM (\xml -> runX (readDocument [] xml >>> entry)) xmls
-  mapM_ showResult ns
-
+  interactions <- (map words . lines) <$> getContents
+  names        <- mapM work interactions
+  mapM_ putStrLn names
 
 
