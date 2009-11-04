91cbcb3bcda99d9a4f1c5e3c50ff4ab80df98d52
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-11-04 18:07:56 -0500

    reorginization/cleanup

--- UniprotXmlParser.hs
+++ UniprotXmlParser.hs
@@ -15,10 +15,10 @@ import Text.XML.HXT.Arrow
 import System.IO.Unsafe
 
 data Entry = Entry {
-      _accession
-    , _entry
-    , _protein
-    , _organism
+      accession
+    , entry
+    , protein
+    , organism
         :: String
     } deriving (Eq, Show)
 
@@ -32,60 +32,56 @@ data Interaction = Interaction {
 class CSV a where
     csv :: a -> String
 
-csvEntry :: Entry -> String
-csvEntry e = intercalate ";" [_accession e, _entry e, _protein e, _organism e]
+instance CSV Entry where
+    csv = csvEntry
+        where csvEntry :: Entry -> String
+              csvEntry e = intercalate ";" [accession e, entry e, protein e, organism e]
 
-instance CSV Entry where csv = csvEntry
+instance CSV Interaction where
+    csv = csvInteraction
+        where csvInteraction :: Interaction -> String
+              csvInteraction i = intercalate ";" [csv $ origin i, csv $ target i, show $ score i]
 
-csvInteraction :: Interaction -> String
-csvInteraction i = intercalate ";" [csv $ origin i, csv $ target i, show $ score i]
-
-instance CSV Interaction where csv = csvInteraction
-
-atTag tag = deep (isElem >>> hasName tag)
-atTag' tag = isElem      >>> hasName tag
-tagData = getChildren    >>> getText
-downFrom top a = proc e -> do
-                   returnA <<< hasName top /> a -< e
-extractFrom mk top a  = arr mk <<< tagData <<< downFrom top a
-extractFromEntry mk = extractFrom mk "entry"
-
-
-entry = atTag "entry" >>>
-        proc e -> do
-          acc   <- single accession    -< e
-          ename <- entryName           -< e
-          p     <- proteinName         -< e
-          oname <- single organismName -< e
-          returnA -< Entry {
-                        _accession = acc
-                      , _entry     = ename
-                      , _protein   = p
-                      , _organism  = oname }
+parseToEntry = atTag "entry" >>>
+               proc e -> do
+                 acc   <- single accession    -< e
+                 ename <- entryName           -< e
+                 p     <- proteinName         -< e
+                 oname <- single organismName -< e
+                 returnA -< Entry {
+                               accession = acc
+                             , entry     = ename
+                             , protein   = p
+                             , organism  = oname }
     
+    where
 
-accession = extractFromEntry id $ hasName "accession"
-entryName = extractFromEntry id $ hasName "name"
-proteinName = proc entry -> do
-                e <- single fullNames -< entry
-                returnA -< e
+      atTag tag            = deep (isElem >>> hasName tag)
+      atTag' tag           = isElem       >>> hasName tag
+      tagData              = getChildren  >>> getText
+      downFrom top a       = hasName top /> a
+      extractFrom mk top a = arr mk <<< tagData <<< downFrom top a
+      extractFromEntry mk  = extractFrom mk "entry"
 
-fullNames = hasName "entry"     /> hasName "protein" >>> atTag "fullName" >>> tagData
 
-organismName = atTag "organism" /> hasName "name"    >>> tagData
+      accession            = extractFromEntry id $ hasName "accession"
+      entryName            = extractFromEntry id $ hasName "name"
+      proteinName          = single fullNames
+      fullNames            = hasName "entry"  /> hasName "protein" >>> atTag "fullName" >>> tagData
+      organismName         = atTag "organism" /> hasName "name"    >>> tagData
 
 
 mkInteraction :: [String] -> IO Interaction
 mkInteraction [ori,tar,prob] =
     let ori' = uniprot ori
         tar' = uniprot tar
-        res = parMap rwhnf (\xml -> runX (readDocument [] xml >>> entry)) [ori', tar']
+        res = parMap rwhnf (\xml -> runX (readDocument [] xml >>> parseToEntry)) [ori', tar']
     in (rearrange . flip mkInteraction' (read prob)) <$> sequence (res `seq` res)
 
         where uniprot id = "http://www.uniprot.org/uniprot/" ++ id ++ ".xml"
               
               rearrange :: Interaction -> Interaction
-              rearrange i = if has _organism "Plasmodium" (origin i)
+              rearrange i = if has organism "Plasmodium" (origin i)
                             then i { origin = target i, target = origin i }
                             else i
 
@@ -99,7 +95,7 @@ interspecies i = if same "Plasmodium" i ||
                  else True
 
     where same :: String -> Interaction -> Bool
-          same s i = hasBoth _organism s (origin i) (target i)
+          same s i = hasBoth organism s (origin i) (target i)
 
 hasBoth :: (Entry -> String) -> String -> Entry -> Entry -> Bool
 hasBoth f s a b = has f s a && has f s b
